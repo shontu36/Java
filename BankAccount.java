b2422f8aea7cb74174844a97b951e69f3207a943
Shazi Ansari <shazijap@gmail..com>
2025-03-26 17:49:12 +0530

    V1

--- BankAccount.java
+++ BankAccount.java
@@ -4,12 +4,14 @@ public class BankAccount {
     double balance;
     String ownerName;
     public void withdraw(double amount) {
-        if (this.balance > 0) {
-            this.balance -= amount;
+        if (balance > 0 && balance>=amount) {
+           balance -= amount;
         }
+        System.out.println("Withdrawn amount is " + balance);
     }
         public void deposit(double amount){
             this.balance+=amount;
-        }
+
+        System.out.println("New amount "+ balance);}
 
 }
---
+++ Client.java
@@ -0,0 +1,9 @@
+package CLass1;
+
+public class Client {
+    public static void main(String[] args){
+        BankAccount ba = new BankAccount();
+        ba.deposit(2000);
+        ba.withdraw(500);
+    }
+}
